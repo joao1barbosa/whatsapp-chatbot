d3c7c63a1f6a268c5cdcfd7ca56f2940d4b30e66
joao1barbosa <joao.rbarbosa3@gmail.com>
2024-08-01 13:28:15 -0300

    feature: integration with dialogflow

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,54 @@
-import express from 'express';
 import { 
-    CloudAdapter, ConfigurationServiceClientCredentialFactory, 
+    CloudAdapter, TurnContext,
     ConfigurationBotFrameworkAuthentication 
 } from 'botbuilder';
-import { EchoBot } from './bot';
+import express from 'express';
+import dialogflow from '@google-cloud/dialogflow';
+import {v4 as uuidv4} from 'uuid';
+import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Auth Config
-const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-    MicrosoftAppId: process.env.MicrosoftAppId,
-    MicrosoftAppPassword: process.env.MicrosoftAppPassword
-});
+//Loading DialogFlow Credentials
+const dialogflowConfig = path.join(__dirname, '..', 'dialogflow-key.json');
+const sessionClient = new dialogflow.SessionsClient({ keyFilename: dialogflowConfig });
+const projectId = 'arteecroche-hnqt';
 
-// const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({}, credentialsFactory);
-
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({}); //Config to Local Tests
-const adapter = new CloudAdapter(botFrameworkAuthentication);
-
-const bot = new EchoBot();
-
+//Express Config
 const server = express();
 server.use(express.json());
+
+//Adapter Config Without Auth
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({});
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+async function handleDialogflowQuery(context: TurnContext) {
+    const sessionPath = sessionClient.projectAgentSessionPath(projectId, uuidv4());
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: context.activity.text,
+                languageCode: 'pt-BR',
+            },
+        },
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    const result = responses[0].queryResult?.fulfillmentText;
+
+    if (result) {
+        await context.sendActivity(result);
+    }
+}
 
 // Endpoint to bot messages
 server.post('/api/messages', (req, res) => {
     adapter.process(req, res, async (context) => {
-        await bot.run(context);
+        if (context.activity.type === 'message') {
+            await handleDialogflowQuery(context);
+        }
     });
 });
 
